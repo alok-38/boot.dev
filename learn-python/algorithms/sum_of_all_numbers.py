2da05b55fda12c3634b5ce2d9c2e0939f2718f2f
Alokananda Y <plantsfortrees@gmail.com>
2024-10-24 06:25:29 +0530

    Wrote my first algorithm to compute the sum

---
+++ learn-python/algorithms/sum_of_all_numbers.py
@@ -0,0 +1,10 @@
+def sum_of_numbers(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+
+list_of_numbers = [45, 78, 1, 6, 89, 100]
+result = sum_of_numbers(list_of_numbers)
+print(result)
+
